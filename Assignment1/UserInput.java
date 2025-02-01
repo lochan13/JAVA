badde858a2438c3a8461c22b03ab74a4be40cfd4
LOCHAN PAUDEL <lochan.paudel.btech2023@sitpune.edu.in>
2025-02-01 04:56:12 +0000

    getMenuChoice function gets created successfully

--- Assignment1/UserInput.java
+++ Assignment1/UserInput.java
@@ -1,5 +1,29 @@
 package Assignment1;
 
+import java.util.Scanner;
+
 public class UserInput {
+    private Scanner scanner;
+
+    public UserInput() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int getMenuChoice() {
+        System.out.println("\nSelect an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Fibonacci Sequence");
+        System.out.println("6. Sum of Array");
+        System.out.println("7. Mean of Array");
+        System.out.println("8. Variance of Array");
+        System.out.println("9. Standard Deviation of Array");
+        System.out.println("10. Exit");
+        System.out.print("Enter your choice: ");
+        return scanner.nextInt();
+    }
+
     
 }
